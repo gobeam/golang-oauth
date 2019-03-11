b1fbe4bbc81bf1f48cde5b5fb5dd53f532358868
Roshan Ranabhat <roshanr@ekbana.com>
2019-03-11 18:05:13 +0545

    change in store item

--- oauth.go
+++ oauth.go
@@ -9,6 +9,7 @@ import (
 	"gopkg.in/oauth2.v3/models"
 	"io"
 	"os"
+	"strconv"
 	"time"
 )
 
@@ -61,14 +62,13 @@ type Store struct {
 type StoreItem struct {
 	ID        int64  `db:"id,primarykey,autoincrement"`
 	ExpiredAt int64  `db:"expired_at"`
-	UserId    string   `db:"user_id"`
+	UserId    string `db:"user_id"`
 	Revoke    bool   `db:"revoke"`
 	Code      string `db:"code,size:255"`
 	Access    string `db:"access,size:255"`
 	Refresh   string `db:"refresh,size:255"`
 	Data      string `db:"data,size:2048"`
 }
-
 
 // NewStore create mysql store instance,
 // config mysql configuration,
@@ -86,7 +86,6 @@ func NewStore(config *Config, gcInterval int) *Store {
 
 	return NewStoreWithDB(db, gcInterval)
 }
-
 
 // NewStoreWithDB create mysql store instance,
 // db sql.DB
@@ -131,7 +130,7 @@ type Config struct {
 }
 
 // NewDefaultStore create mysql store instance
-func NewDefaultStore(config *Config) *Store{
+func NewDefaultStore(config *Config) *Store {
 	return NewStore(config, 0)
 }
 
@@ -149,7 +148,7 @@ func (s *Store) gc() {
 
 func (s *Store) clean() {
 	now := time.Now().Unix()
-	query := fmt.Sprintf("SELECT COUNT(*) FROM %s WHERE expired_at<=? OR (code='' AND access='' AND refresh='')", s.tableName)
+	query := fmt.Sprintf("SELECT COUNT(*) FROM %s WHERE expired_at<=? OR (code='' AND access='' AND refresh='')", s.accessTable)
 	n, err := s.db.SelectInt(query, now)
 	if err != nil || n == 0 {
 		if err != nil {
@@ -158,7 +157,7 @@ func (s *Store) clean() {
 		return
 	}
 
-	_, err = s.db.Exec(fmt.Sprintf("DELETE FROM %s WHERE expired_at<=? OR (code='' AND access='' AND refresh='')", s.tableName), now)
+	_, err = s.db.Exec(fmt.Sprintf("DELETE FROM %s WHERE expired_at<=? OR (code='' AND access='' AND refresh='')", s.accessTable), now)
 	if err != nil {
 		s.errorf(err.Error())
 	}
@@ -173,29 +172,29 @@ func (s *Store) errorf(format string, args ...interface{}) {
 
 // Create create and store the new token information
 func (s *Store) Create(info oauth2.TokenInfo) error {
-	buf, _ := jsoniter.Marshal(info)
-	item := &StoreItem{
-		Data: string(buf),
-	}
-	item.UserId = info.GetUserID()
-	if code := info.GetCode(); code != "" {
-		item.Code = code
-		item.ExpiredAt = info.GetCodeCreateAt().Add(info.GetCodeExpiresIn()).Unix()
-	} else {
-		item.Access = info.GetAccess()
-		item.ExpiredAt = info.GetAccessCreateAt().Add(info.GetAccessExpiresIn()).Unix()
+	//buf, _ := jsoniter.Marshal(info)
+	item := &OauthAccessTokens{}
+	i, err := strconv.ParseInt(info.GetUserID(), 10, 64)
+	if err != nil {
 
-		if refresh := info.GetRefresh(); refresh != "" {
-			item.Refresh = info.GetRefresh()
-			item.ExpiredAt = info.GetRefreshCreateAt().Add(info.GetRefreshExpiresIn()).Unix()
-		}
+	}
+	item.UserId = i
+	cid, err := strconv.ParseInt(info.GetClientID(), 10, 64)
+	if err != nil {
+
+	}
+	item.ClientId = cid
+	item.ExpiredAt = info.GetAccessCreateAt().Add(info.GetAccessExpiresIn()).Unix()
+	if refresh := info.GetRefresh(); refresh != "" {
+		item.Refresh = info.GetRefresh()
+		item.ExpiredAt = info.GetRefreshCreateAt().Add(info.GetRefreshExpiresIn()).Unix()
 	}
 
 	return s.db.Insert(item)
 }
 
 func (s *Store) ClearAccessToken(info oauth2.TokenInfo) error {
-	query := fmt.Sprintf("DELETE FROM %s WHERE user_id=?", s.tableName)
+	query := fmt.Sprintf("DELETE FROM %s WHERE user_id=?", s.accessTable)
 	_, err := s.db.Exec(query, info.GetUserID())
 	if err != nil && err == sql.ErrNoRows {
 		return nil
@@ -204,7 +203,7 @@ func (s *Store) ClearAccessToken(info oauth2.TokenInfo) error {
 }
 
 func (s *Store) RevokeAccessTokens(id string) error {
-	query := fmt.Sprintf("UPDATE %s SET `revoke`=? WHERE user_id IN (?)", s.tableName)
+	query := fmt.Sprintf("UPDATE %s SET `revoke`=? WHERE user_id IN (?)", s.accessTable)
 	_, err := s.db.Exec(query, 1, id)
 	if err != nil && err == sql.ErrNoRows {
 		return nil
@@ -214,7 +213,7 @@ func (s *Store) RevokeAccessTokens(id string) error {
 
 // RemoveByCode delete the authorization code
 func (s *Store) RemoveByCode(id string) error {
-	query := fmt.Sprintf("UPDATE %s SET code='' WHERE code=? LIMIT 1", s.tableName)
+	query := fmt.Sprintf("UPDATE %s SET code='' WHERE code=? LIMIT 1", s.accessTable)
 	_, err := s.db.Exec(query, id)
 	if err != nil && err == sql.ErrNoRows {
 		return nil
@@ -224,7 +223,7 @@ func (s *Store) RemoveByCode(id string) error {
 
 // RemoveByAccess use the access token to delete the token information
 func (s *Store) RemoveByAccess(access string) error {
-	query := fmt.Sprintf("UPDATE %s SET access='', refresh='' WHERE access=? LIMIT 1", s.tableName)
+	query := fmt.Sprintf("UPDATE %s SET access='', refresh='' WHERE access=? LIMIT 1", s.accessTable)
 	_, err := s.db.Exec(query, access)
 	if err != nil && err == sql.ErrNoRows {
 		return nil
@@ -234,7 +233,7 @@ func (s *Store) RemoveByAccess(access string) error {
 
 // RemoveByRefresh use the refresh token to delete the token information
 func (s *Store) RemoveByRefresh(refresh string) error {
-	query := fmt.Sprintf("UPDATE %s SET refresh='', access='' WHERE refresh=? LIMIT 1", s.tableName)
+	query := fmt.Sprintf("UPDATE %s SET refresh='', access='' WHERE refresh=? LIMIT 1", s.accessTable)
 	_, err := s.db.Exec(query, refresh)
 	if err != nil && err == sql.ErrNoRows {
 		return nil
@@ -254,7 +253,7 @@ func (s *Store) GetByCode(code string) (oauth2.TokenInfo, error) {
 		return nil, nil
 	}
 
-	query := fmt.Sprintf("SELECT * FROM %s WHERE code=? LIMIT 1", s.tableName)
+	query := fmt.Sprintf("SELECT * FROM %s WHERE code=? LIMIT 1", s.accessTable)
 	var item StoreItem
 	err := s.db.SelectOne(&item, query, code)
 	if err != nil {
@@ -272,7 +271,7 @@ func (s *Store) GetByAccess(access string) (oauth2.TokenInfo, error) {
 		return nil, nil
 	}
 
-	query := fmt.Sprintf("SELECT * FROM %s WHERE access=? LIMIT 1", s.tableName)
+	query := fmt.Sprintf("SELECT * FROM %s WHERE access=? LIMIT 1", s.accessTable)
 	var item StoreItem
 	err := s.db.SelectOne(&item, query, access)
 	if err != nil {
@@ -290,7 +289,7 @@ func (s *Store) GetByRefresh(refresh string) (oauth2.TokenInfo, error) {
 		return nil, nil
 	}
 
-	query := fmt.Sprintf("SELECT * FROM %s WHERE refresh=? LIMIT 1", s.tableName)
+	query := fmt.Sprintf("SELECT * FROM %s WHERE refresh=? LIMIT 1", s.accessTable)
 	var item StoreItem
 	err := s.db.SelectOne(&item, query, refresh)
 	if err != nil {
